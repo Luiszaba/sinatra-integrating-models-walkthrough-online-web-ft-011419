63468ef4b86a014abcf4e55cd329c625596b2fb5
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 21:34:11 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,2 +1,24 @@
 # Your TextAnalyzer model code will go here.
 
+class TextAnalyzer
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = text.split(" ")
+    words.count
+  end
+  
+  def count_of_vowels
+    text.scan(/[aeiou]/).count
+  end
+  
+  def count_of_consonants
+    text.scan(/[bcdfgfhjklmnprstvwxyz]/).count
+  end
+  
+  def 
+    
