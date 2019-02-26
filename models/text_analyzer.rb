189511c6dd65aeaae8e1786b284ca5cda31ef21b
Luis Zabala <luis.zabala076@gmail.com>
2019-02-26 22:18:29 +0000

    Automatically backed up by Learn

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -22,10 +22,10 @@ class TextAnalyzer
   
   def most_used_letter
     s1 = text.gsub(/[^a-z]/, '')
-    #remove 
     arr = s1.split('')
     arr1 = arr.uniq
     arr2 = {}
+    
     arr1.map do |c|
       arr2[c] = arr.count(c)
     end
